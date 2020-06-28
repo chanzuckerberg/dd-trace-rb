002c3678e2c7f14a5c66c0a0a58947027decc9cb
David Elner <david@davidelner.com>
2020-06-27 23:24:14 -0400

    WIP: Added: Profiler & configuration.

---
+++ lib/ddtrace/profiling/profiler.rb
@@ -0,0 +1,34 @@
+require 'ddtrace/profiling/collectors/stack'
+require 'ddtrace/profiling/exporter'
+require 'ddtrace/profiling/recorder'
+require 'ddtrace/profiling/scheduler'
+require 'ddtrace/profiling/transport/io'
+
+module Datadog
+  # Generates profiles and transmits them to Datadog
+  class Profiler
+    attr_reader \
+      :collectors,
+      :scheduler
+
+    def initialize(collectors, scheduler)
+      @collectors = collectors
+      @scheduler = scheduler
+    end
+
+    def start
+      collectors.each(&:start)
+      scheduler.start
+    end
+
+    def shutdown!
+      collectors.each do |collector|
+        collector.enabled = false
+        collector.stop(true)
+      end
+
+      scheduler.enabled = false
+      scheduler.stop(true)
+    end
+  end
+end
